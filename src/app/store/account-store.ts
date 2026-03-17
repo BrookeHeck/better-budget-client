ce0f2503caf3deb00d271b2d72393a49e008b7b5
BrookeHeck <b19heck@gmail.com>
2026-03-16 20:54:36 -0400

    add account store and crud methods

--- src/app/store/account-store.ts
+++ src/app/store/account-store.ts
@@ -1,3 +1,50 @@
-// const type AccountState = {
-//
-// }
+import {Account} from '../model/account/Account';
+import {patchState, signalStore, withComputed, withMethods, withState} from '@ngrx/signals';
+import {inject} from '@angular/core';
+import {AccountRequests} from '../service/http-requests/account-requests';
+
+type AccountState = {
+  accounts: Account[],
+  loading: boolean,
+}
+
+const initialState: AccountState = {
+  accounts: [],
+  loading: false,
+}
+
+const accountStore = signalStore(
+  {providedIn: "root"},
+  withState(initialState),
+  withMethods((store, accountRequestService = inject(AccountRequests)) => ({
+    async loadAllAccounts(userId: number) {
+      patchState(store, {loading: true});
+      const accounts = await accountRequestService.getUserAccounts(userId);
+      patchState(store, {loading: false, accounts});
+    },
+    async createAccount(account: Account) {
+      patchState(store, {loading: true});
+      const created = await accountRequestService.createAccount(account);
+      patchState(store, state => ({
+        loading: false,
+        accounts: [...state.accounts, created]
+      }))
+    },
+    async updateAccount(account: Account) {
+      patchState(store, {loading: true});
+      const updated = await accountRequestService.updateAccount(account);
+      patchState(store, state => {
+        const accounts = state.accounts.map(a => a.accountId === account.accountId ? updated : a);
+        return {loading: false, accounts};
+      })
+    },
+    async deleteAccount(accountId: Account) {
+      patchState(store, {loading: true});
+      await accountRequestService.deleteAccount(accountId);
+      patchState(store, state => {
+        const accounts = state.accounts.filter(a => a.accountId !== accountId);
+        return {loading: false, accounts};
+      })
+    }
+  }))
+)
