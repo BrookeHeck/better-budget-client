570eedab87ce6fe35d68f7527a7d64cb02c69e74
BrookeHeck <b19heck@gmail.com>
2026-03-18 19:18:20 -0400

    add transaction store and crud methods

--- src/app/page/transactions/create-transaction/create-transaction.ts
+++ src/app/page/transactions/create-transaction/create-transaction.ts
@@ -2,5 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'create-transaction',
-
+  imports: [],
+  templateUrl: 'create-transaction.html'
 })
+export class CreateTransaction {
+
+}
---
+++ src/app/store/transaction-store.ts
@@ -0,0 +1,49 @@
+import {Transaction} from '../model/transaction/transaction';
+import {patchState, signalStore, withMethods, withState} from '@ngrx/signals';
+import {inject} from '@angular/core';
+import {TransactionRequests} from '../service/http-requests/transaction-requests';
+
+type TransactionState = {
+  transactions: Transaction[],
+  loading: boolean
+}
+
+const initialState: TransactionState = {
+  transactions: [],
+  loading: false,
+}
+
+export const TransactionStore = signalStore(
+  {providedIn: 'root'},
+  withState(initialState),
+  withMethods((store, transactionService = inject(TransactionRequests)) => ({
+    async loadAllTransactions(userId: number, startDate?: Date, endDate?: Date) {
+      patchState(store, {loading: true});
+      const transactions = await transactionService.getTransactionsWithinDateRange(userId, startDate, endDate);
+      patchState(store, {loading: false, transactions});
+    },
+    async createTransaction(transaction: Transaction) {
+      patchState(store, {loading: true});
+      const created = await transactionService.createTransaction(transaction);
+      patchState(store, state => ({
+        loading: false, transactions: [...state.transactions, created]
+      }))
+    },
+    async updateTransaction(transaction: Transaction) {
+      patchState(store, {loading: true});
+      const updated = await transactionService.updateTransaction(transaction);
+      patchState(store, state => {
+        const transactions = state.transactions.map(t => t.transactionId === updated.transactionId ? updated : t);
+        return {loading: false, transactions};
+      })
+    },
+    async deleteTransaction(transactionId: number) {
+      patchState(store, {loading: true});
+      await transactionService.deleteTransaction(transactionId);
+      patchState(store, state => {
+        const transactions = state.transactions.filter(t => t.transactionId !== transactionId);
+        return {loading: false, transactions};
+      })
+    }
+  }))
+)
